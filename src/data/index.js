1119f8dd2c58334f293530ccda68dd171c161544
freakssha <lestreng.begi@gmail.com>
2021-12-04 07:44:35 +0300

    новые тестовые данные

--- src/data/index.js
+++ src/data/index.js
@@ -34,4 +34,26 @@ export const historyListData = [
         content:
             'Доля брака в погрузке номер 0132846746 (Санкт-Петербург - Новосибирск) в пределах нормы (>7.5%)',
     },
+    {
+        href: '',
+        title: dt.getFullYear() + "/" + (dt.getMonth() + 1) + "/" + dt.getDate(),
+        tag: "Пункт разгрузки-3",
+        avatar: 'https://joeschmoe.io/api/v1/random',
+        uploaded: 'https://sun9-78.userapi.com/impg/OAaBWUIzRm863l-YvHq7mijZVopuvls1v-OVWw/eOC3v50QRWQ.jpg?size=256x257&quality=96&sign=81ec4173b572220e3390194a7ac38180&type=album',
+        description:
+            'Дата, доля брака и информация о поставке определены и занесены в смету автоматически.',
+        content:
+            'Доля брака в погрузке номер 0009846746 (Казань - Новосибирск) в пределах нормы (>5%)',
+    },
+    {
+        href: '',
+        title: dt.getFullYear() + "/" + (dt.getMonth() + 1) + "/" + dt.getDate(),
+        tag: "Пункт разгрузки-1",
+        avatar: 'https://joeschmoe.io/api/v1/random',
+        uploaded: 'https://sun9-61.userapi.com/impg/gNferGWLsTJYhv2NzVU7bIAoyQKYEUlC6892iw/DKZEOkNvNIE.jpg?size=293x204&quality=96&sign=84de9688c790a536f75041016cabedbb&type=album',
+        description:
+            'Дата, доля брака и информация о поставке определены и занесены в смету автоматически.',
+        content:
+            'Доля брака в погрузке номер 0132846746 (Санкт-Петербург - Новосибирск) в пределах нормы (>7.5%)',
+    },
 ]
